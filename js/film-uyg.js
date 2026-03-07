38b03be2fe528de74ee5387c8480757984f1eb9c
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2026-03-07 12:35:31 +0300

    film uygulaması API bilgileri yapıldı

--- js/film-uyg.js
+++ js/film-uyg.js
@@ -1,38 +1,38 @@
-const APIkey = "3e63fb5bdd5cafa1ecff609b8996f736";
+const APIkey = "60ab165f0d6c3bb6d32d6a2a96f966e7";
 const baseURL = "https://api.themoviedb.org/3";
-const myUrl = `${baseURL}/movie/popular?${APIkey}&page=1`;
+const myUrl = `${baseURL}/movie/popular?api_key=${APIkey}&page=1`;
 const imgPath = "https://image.tmdb.org/t/p/original";
 
-const searchAPI = `${baseURL}/search/movie?query=${searchMovie}`;
-
 const form = document.getElementById("form");
-const search = document.getElementById("search");
+const search = form.elements.search;
 const filmArea = document.querySelector(".filmArea");
+const searchAPI = `${baseURL}/search/movie?query=${search}`;
 
 allMovies(myUrl);
 
 async function allMovies(url) {
   const response = await fetch(url);
-  const responseData = response.json();
+  const responseData = await response.json();
+  console.log(responseData.results);
   filmleriGoster(responseData.results);
 }
 
 async function filmleriGoster(film) {
   filmArea.innerHTML = "";
-  film.forEach((item) => {
+   film.forEach((item) => {
     const { title, overview, release_date, vote_average, poster_path } = item;
     const filmElement = document.createElement("div");
-    filmElement.classList("movie");
+    filmElement.classList.add("movie");
     filmElement.innerHTML = `
-      <img src="${imgPath}+poster_path" alt="${title}"/>
+      <img src="${imgPath}${poster_path}" alt="${title}"/>
       <div class="movie-info">
       <h3> ${title}
-      <span>
-      "${filmOranı(vote_average)}">${vote_average}"</span>
+      <span>filmOranı
+      "${vote_average}">${vote_average}"</span>
       </h3>
       </div>
-      <div>
-      <h3>
+      <div class="overview">
+      <h3 ">
       Açıklama:</h3>"${overview}"</div>
       `;
     filmArea.appendChild(filmElement);
@@ -44,6 +44,7 @@ form.addEventListener("submit", (e) => {
   const arananFilm = search.value;
   if (arananFilm) {
     allMovies(searchAPI + arananFilm);
+    console.log(arananFilm);
     search.value = "";
   }
 });
